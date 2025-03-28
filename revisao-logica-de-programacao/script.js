4199a913428ac0d4d6f3d24857476923b3c7e47a
AndreRichartzFarias <andre_r_farias@estudante.sesisenai.org.br>
2025-03-28 12:29:44 -0300

    exercicios terminados

--- revisao-logica-de-programacao/script.js
+++ revisao-logica-de-programacao/script.js
@@ -78,4 +78,90 @@ window.balancoFinanceiro = () => {
     
 };
 
+window.organizarNumeros = () => {
+    let i = 0;
+    let numeroAtual = 0;
+    let continuar = '';
+    let numeros = [];
+    while (continuar !== '0') {
+        numeroAtual = prompt(`Informe o número ${i+1}`);
+        while (isNaN(numeroAtual)) {
+            numeroAtual = prompt('Por favor, informe um número válido!');
+        }
+        numeros.push(parseFloat(numeroAtual));
+        continuar = prompt(`Informe "0" para encerrar ou qualquer tecla para continuar`);
+        i++;
+    }
+    numeros.sort((a, b) => b - a);
+    alert(`Os números ordenados são: ${numeros}`);
+};
+
+window.parParaImpar = () => {
+let i = 0;
+let numeroAtual = 0;
+let continuar = '';
+let pares = [];
+let impares = [];
+
+while (continuar !== '0') {
+    numeroAtual = prompt(`Informe o número ${i+1}`);
+    while (isNaN(numeroAtual)) {
+        numeroAtual = prompt('Por favor, informe um número válido!');
+    }
+    if (numeroAtual % 2 == 0) {
+        impares.push(parseFloat(numeroAtual)+1);
+    } else {
+        pares.push(parseFloat(numeroAtual)+1);
+    }
+    continuar = prompt(`Informe "0" para encerrar ou qualquer tecla para continuar`);
+    i++;
+
+}
+alert(`Os números pares são: ${pares}`);
+alert(`Os números ímpares são: ${impares}`);
+}
+
+window.vogalOuConsoante = () => {
+    let i = 0;
+    let letraAtual = '';
+    let continuar = '';
+    let vogais = [];
+    let consoantes = [];
+    let vogal = ['a', 'e', 'i', 'o', 'u'];
+    while (continuar !== '0') {
+        letraAtual = prompt(`Informe a letra ${i+1}`);
+        while (letraAtual.length > 1) {
+            letraAtual = prompt('Por favor, informe apenas uma letra!');
+        }
+        if (vogal.includes(letraAtual)) {
+            vogais.push(letraAtual);
+        } else {
+            consoantes.push(letraAtual);
+        }
+        continuar = prompt(`Informe "0" para encerrar ou qualquer tecla para continuar`);
+        i++;
+    }
+    alert(`As vogais são: ${vogais}`);
+    alert(`As consoantes são: ${consoantes}`);
+}
+
+window.precoPicole = () => {
+    let picoles = ["Chocolate", "Morango", "Creme", "Manga", "Melancia", "Vanilla Ice", "Céu Azul", "Brownie", "Hawaiano"];
+    let precos = [1.50, 2.50, 2.50, 3.20, 3.40, 3.00, 3.60, 4.00, 5.00];
+    let posicao = -1;
+    alert(`Os sabores disponíveis são:\n${picoles.map((picole, index) => `${index + 1}: ${picole}`).join('\n')}`);
+    posicao = prompt('Informe a posição do picolé');
+
+    while (isNaN(posicao) || posicao < 1 || posicao > picoles.length) {
+        alert(`Os sabores disponíveis são:\n${picoles.map((picole, index) => `${index + 1}: ${picole}`).join('\n')}`);
+        posicao = prompt('Por favor, informe uma posição válida');
+    }
+
+    posicao = parseInt(posicao) - 1;
+
+    if (posicao !== -1) {
+        alert(`R$${precos[posicao]}`);
+    }
+}
+
 });
